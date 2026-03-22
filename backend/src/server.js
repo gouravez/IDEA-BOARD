9ba076269fe66a43e8256e596a4159129c18d740
Gourav <junejagourav2006@gmail.com>
2026-03-22 19:45:13 +0530

    Url update

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ import rateLimiter from "./middleware/rateLimiter.js";
 
 // middleware -> function that runs in middle between request and response
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://ideaboardgouravez.vercel.app/"],
+  origin: ["http://localhost:5173", "https://ideaboardgouravez.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
