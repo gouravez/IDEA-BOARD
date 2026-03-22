befafc90b6c9ee04075760bad71c88af78f7fbbd
Gourav <junejagourav2006@gmail.com>
2026-03-22 16:04:57 +0530

    cors fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,21 +12,25 @@ const PORT = process.env.PORT;
 import rateLimiter from "./middleware/rateLimiter.js";
 
 // middleware -> function that runs in middle between request and response
-app.use(
-  cors({
-    origin: ["http://localhost:5173","https://idea-board-e5lk.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "https://idea-board-e5lk.vercel.app",
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(express.json()); // middleware to PARSE json bodies : req.body
-app.use(rateLimiter);
 
 app.use((req, res, next) => {
   next();
 });
 
 app.use("/api/notes", notesRoutes);
+app.use(rateLimiter);
 
 connectDB().then(() => {
   app.listen(PORT, () => {
